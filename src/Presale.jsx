58385c0ce629fe3d96810b5cf7650d07fd236d28
Saurabh <saurabh@Saurabhs-Laptop.local>
2025-04-14 11:20:25 +0100

    Right contract address for USDC

--- src/Presale.jsx
+++ src/Presale.jsx
@@ -58,7 +58,7 @@ export default function Presale() {
       
       if (currency === 'USDC' || currency === 'USDT') {
         const stablecoinAddress = currency === 'USDC' 
-          ? '0xA0b86991C6218B36c1d19D4a2e9Eb0cE3606EB48' 
+          ? '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' 
           : '0xdAC17F958D2ee523a2206206994597C13D831ec7';
 
         const stablecoinABI = [
